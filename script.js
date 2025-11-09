548be4181707b989b439318911c5445f92ca574e
NestorAndres1215 <atiroandres@gmail.com>
2025-11-08 22:38:21 -0500

    subiendo

--- script.js
+++ script.js
@@ -276,8 +276,6 @@ window.addEventListener("scroll", () => {
   }
 });
 
-volverArriba.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
+
 
 obtenerRazas();
